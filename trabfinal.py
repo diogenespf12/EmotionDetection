0ab39c67f210b33f95d6e93e72a55461faff0ccd
Lucas <lucasbuhler99@gmail.com>
2023-06-22 10:56:58 -0300

    v2

--- trabfinal.py
+++ trabfinal.py
@@ -1,17 +1,17 @@
 import os
 import cv2
 import numpy as np
-from keras.preprocessing import image
 import warnings
 warnings.filterwarnings("ignore")
+import matplotlib.pyplot as plt
+import h5py as h5
+from PIL import Image
+from keras.preprocessing import image
 from keras.utils.image_utils import load_img, img_to_array 
 from keras.models import load_model
-import matplotlib.pyplot as plt
 
 # load model
-filepath = os.path.join(os.getcwd(), "best_model.h5").encode("utf8")
-model = load_model(filepath)
-
+model = load_model("best_model.h5")
 
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -30,10 +30,10 @@ while True:
         cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
         roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
         roi_gray = cv2.resize(roi_gray, (224, 224))
-        img_pixels = image.img_to_array(roi_gray)
+        img_pixels = img_to_array(roi_gray)
         img_pixels = np.expand_dims(img_pixels, axis=0)
         img_pixels /= 255
-
+        
         predictions = model.predict(img_pixels)
 
         # find max indexed array
@@ -45,7 +45,7 @@ while True:
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ', resized_img)
+    cv2.imshow('Facial emotion analysis (Press q to exit) ', resized_img)
 
     if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
         break
