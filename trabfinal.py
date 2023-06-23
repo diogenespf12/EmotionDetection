707b02de914adb8e865eaef0eb5db43a81502d7a
Diógenes P.Fernandes <73259579+diogenespf12@users.noreply.github.com>
2023-06-23 16:24:21 -0300

    Update trabfinal.py

--- trabfinal.py
+++ trabfinal.py
@@ -15,11 +15,12 @@ model = load_model("best_model.h5")
 
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-
+# Captura de Webcam
 cap = cv2.VideoCapture(0)
 
+
 while True:
-    ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
+    ret, test_img = cap.read()  # captura os frames e retorna o valor booleano e a imagem capturada
     if not ret:
         continue
     gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
@@ -28,16 +29,16 @@ while True:
 
     for (x, y, w, h) in faces_detected:
         cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
-        roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
+        roi_gray = gray_img[y:y + w, x:x + h]  # Regiao de corte de interesse, ou seja, area da face da imagem
         roi_gray = cv2.resize(roi_gray, (224, 224))
-        img_pixels = img_to_array(roi_gray)
+        img_pixels = image.img_to_array(roi_gray)
         img_pixels = np.expand_dims(img_pixels, axis=0)
         img_pixels /= 255
-        
-        predictions = model.predict(img_pixels)
 
-        # find max indexed array
-        max_index = np.argmax(predictions[0])
+        pred = model.predict(img_pixels)
+
+        # find max indexed array 
+        max_index = np.argmax(pred[0])
 
         emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
         predicted_emotion = emotions[max_index]
@@ -45,10 +46,30 @@ while True:
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis (Press q to exit) ', resized_img)
+    cv2.imshow('Emotion Detection', resized_img)
 
-    if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+    if cv2.waitKey(10) == ord('q'):  # Espera ate a tecla Q ser pressionada
         break
+
+"""Input de imagem
+path = "train/Angry/download.jpg"
+img = load_img(path, target_size=(224,224) )
+i = img_to_array(img)/255
+input_arr = np.array([i])
+input_arr.shape
+
+pred = np.argmax(model.predict(input_arr))
+
+print(f" A emocao da imagem e de {op[pred]}")
+
+#Interface do OpenCV
+cv2.imshow('Emotion Detection',input_arr[0])
+
+#Interface do Matplotlib
+#plt.imshow(input_arr[0])
+#plt.title("Emotion Detection")
+#plt.show()
+"""
 
 cap.release()
 cv2.destroyAllWindows
